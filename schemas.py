7c3cecbb744de7e0341d78b9728f37ee649e4dc1
sydneyIdundun <sydney.idundun@gmail.com>
2023-06-06 23:35:12 +0400

    todo: Restructured validation Schemas :construction:

--- schemas.py
+++ schemas.py
@@ -1,44 +1,74 @@
 from pydantic import BaseModel
+from typing import List
 
-class BaseUser(BaseModel):
-    # this is the generic class for a user 
-    first_name: str
-    last_name: str
-    email: str
-
-
-class CreateUser(BaseUser):
-    password: str
-
-class UpdateUserPassword(BaseModel):
-    password: str | None = None
-
-class UpdateUser(BaseUser):
+# VOTE SCHEMAS
+class BaseVote(BaseModel):
+    voterId: str
+    pollId: str
+    optionId: str
     pass
 
-class User(BaseUser):
-    id: str
-    polls: list[str]
+class CreateVote(BaseVote):
+    pass
 
+class Vote(BaseVote):
+    id: str
+    class Config:
+        orm_mode = True
+
+
+# OPTION SCHEMAS
+class BaseOption(BaseModel):
+    value: str
+    pass
+
+class CreateOption(BaseOption):
+    pollId: str
+    pass
+
+class Options(BaseModel):
+    votes: List[Vote]
+    class Config:
+        orm_mode = True
+
+
+# POLLS SCHEMA 
 class BasePoll(BaseModel):
     question: str
-    private: bool = False
-    protected: bool = False
-    closed: bool = False
-    owner_id: str
-    options: list[str] = []
-    
+    private: bool
+    protected: bool 
+    closed: bool 
 
 class CreatePoll(BasePoll):
     pass
 
-class UpdatePoll(BasePoll):
-    pass
+class Poll(BasePoll):
+    id: str
+    owner_id: str
+    created_at: str
+    options = List[Options]
+    votes = List[Vote]
+    
+    class Config:
+        orm_mode = True
 
-class BaseVote(BaseModel):
-    pass
+# USER SCHEMAS
+class BaseUser():
+    first_name : str
+    last_name: str
+    email: str 
 
-class CreateVote(BaseVote):
-    voterId: str
-    pollId: str
-    optionId: str
+class CreateUser(BaseUser):
+    password: str
+
+class User(BaseUser):
+    id: str
+    polls: List[Poll]
+    votes: List[Vote]
+    created_at: str
+    
+    class Config:
+        orm_mode = True
+
+
+
