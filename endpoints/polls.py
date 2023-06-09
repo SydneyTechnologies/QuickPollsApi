47eabcfac8e5107a41cb2817c385bfc80208286c
sydneyIdundun <sydney.idundun@gmail.com>
2023-06-09 23:02:11 +0400

    chore: Added a new endpoint file polls
    In order to organize all the endpoints, I made user of fastapis router and tags to seperate endpoints and give the project some structure

---
+++ endpoints/polls.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter, HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+import schemas, crud, utils, tables
+
+router = APIRouter()
+
+@router.post("/polls", tags=["Poll"], summary="Create a poll for this user")
+def createPoll(pollData: schemas.CreatePoll, user: tables.User = Depends(utils.get_current_user), db = Depends(utils.get_db)):
+    poll = pollData.copy()
+    poll = poll.dict()
+    poll.update({"owner_id": user.id})
+    new_poll = tables.Poll(**poll)
+    new_poll = crud.add_to_db(new_poll, db)
+    if new_poll:
+        return schemas.Poll.from_orm(new_poll)
+    
+@router.put("/polls/{pollId}", tags=["Poll"], summary="Update poll by pollId")
+def updatePoll(pollId:str):
+    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+
+
+@router.delete("/polls/{pollId}", tags=["Poll"], summary="Delete poll by pollId")
+def deletePoll(pollId:str):
+    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+
+@router.get("/polls",  tags=["Poll"], summary="List all the polls in the database")
+def listPolls(db = Depends(utils.get_db)):
+    polls: list[tables.Poll] | None = crud.get_db_objects(object=tables.Poll, db=db)
+    if polls :
+        for p in polls: 
+            print(schemas.Poll.from_orm(p))
+        return [ schemas.Poll.from_orm(poll) for poll in polls]
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
--- main.py
+++ main.py
@@ -2,31 +2,13 @@ from fastapi import FastAPI, Depends, HTTPException, status
 import tables, crud, schemas
 from databaseConnection import engine
 from utils import *
-from endpoints import user
+from endpoints import user, polls
 
 tables.BaseTable.metadata.create_all(bind=engine)
 DESCRIPTION = "The QuickPolls API is a powerful and interactive platform built with FastAPI that allows users to create and participate in polls, while providing real-time live voting statistics. 🗳️📊"
 app = FastAPI(title="QuickPollsApi", description=DESCRIPTION)
 
 app.include_router(user.router)
+app.include_router(polls.router)
 
-# @app.post("/polls")
-# def createPoll(pollData: schemas.CreatePoll, user: tables.User = Depends(get_current_user), db = Depends(get_db)):
-#     poll = pollData.copy()
-#     poll = poll.dict()
-#     poll.update({"owner_id": user.id})
-#     new_poll = tables.Poll(**poll)
-#     new_poll = crud.add_to_db(new_poll, db)
-#     if new_poll:
-#         return schemas.Poll.from_orm(new_poll)
-
-# @app.get("/polls")
-# def listPolls(db = Depends(get_db)):
-#     polls: list[tables.Poll] | None = crud.get_db_objects(object=tables.Poll, db=db)
-#     if polls :
-#         for p in polls: 
-#             print(schemas.Poll.from_orm(p))
-#         return [ schemas.Poll.from_orm(poll) for poll in polls]
-#     else:
-#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
